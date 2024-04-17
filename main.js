1e729bbca9adaba00103428d12f6b085f920e7f4
malwinakochman <malwinakochman@gmail.com>
2024-04-17 12:28:47 +0200

    Co-authored-by: lismalgorzata <lismalgorzata@users.noreply.github.com>

--- main.js
+++ main.js
@@ -6,28 +6,3 @@ window.onload = () => {
                .register('./service-worker.js');
     }
   }
-
-  document.addEventListener('DOMContentLoaded', function() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-
-            var map = L.map('map').setView([latitude, longitude], 13);
-
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-
-            var marker = L.marker([latitude, longitude]).addTo(map);
-            marker.bindPopup("Jesteś tutaj!").openPopup();
-
-        }, function(error) {
-            console.error("Error getting location: ", error);
-            alert('Error getting location: ' + error.message);
-        });
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
-});
