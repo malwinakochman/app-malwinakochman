607c07aaf3da036dc63cc530fbfddebc586d9ba3
malwinakochman <malwinakochman@gmail.com>
2024-04-17 12:18:49 +0200

    changes23

--- main.js
+++ main.js
@@ -6,3 +6,30 @@ window.onload = () => {
                .register('./service-worker.js');
     }
   }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            // Inicjalizacja mapy Leaflet na pozycji użytkownika
+            var map = L.map('map').setView([latitude, longitude], 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            // Dodanie markera na pozycji użytkownika
+            var marker = L.marker([latitude, longitude]).addTo(map);
+            marker.bindPopup("Jesteś tutaj!").openPopup();
+
+        }, function(error) {
+            console.error("Error getting location: ", error);
+            alert('Error getting location: ' + error.message);
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+});
